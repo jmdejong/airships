3fbd6707db4a63c0b8c63857592bdb385e5145ae
troido <troido@protonmail.com>
2025-05-04 12:19:16 +0200

    fix ballast buttons

--- code/button3d.gd
+++ code/button3d.gd
@@ -4,12 +4,6 @@ extends Area3D
 
 signal pressed;
 
-func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	return
-	print(event)
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		pressed.emit()
-
 func mouseover_description() -> String:
 	return description
 
