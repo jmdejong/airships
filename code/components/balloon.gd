6ed71f4d3d7101e9a9e3126ed7bb0751d5d0ea43
troido <troido@protonmail.com>
2026-04-08 23:41:54 +0200

    balloon improvements

--- code/components/balloon.gd
+++ code/components/balloon.gd
@@ -17,10 +17,11 @@ extends BaseComponent
 		if has_node("MeshInstance3D"):
 			$MeshInstance3D.mesh.height = val
 @export var filling_density: float = 0.1
-@export var envelope_mass: float = 0.1
+@export var envelope_density: float = 0.5 # kg / m^2
+@export var structure_mass: float = 10
 
 func _ready() -> void:
 	var tube: float = length - radius * 2
 	volume = 4.0/3 * PI * radius * radius * radius + tube * PI * radius * radius
 	var surface = 4 * PI * radius * radius + 2 * PI * radius * tube
-	mass_ = filling_density * volume + surface * envelope_mass
+	mass_ = filling_density * volume + surface * envelope_density
